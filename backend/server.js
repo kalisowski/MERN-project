3de75f8e08effc0173e3e9f53e387b16a54ac10f
kalisowski <lisekgda3@gmail.com>
2022-12-19 22:50:32 +0100

    added conntection to database, middleware and routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
 const port = process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
-app.get('/api/cocktails', (req, res) => {
-  res.json({ msg: 'Tu beda koktajle' });
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+app.use('/api/cocktails', require('./routes/cocktailsRoutes'));
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server running on port ${port}`));
