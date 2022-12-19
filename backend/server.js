f64c80f393fb54e6eb2248ec89371c8cac11ebaf
kalisowski <lisekgda3@gmail.com>
2022-12-19 19:39:25 +0100

    backend init

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/cocktails', (req, res) => {
+  res.json({ msg: 'Tu beda koktajle' });
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
