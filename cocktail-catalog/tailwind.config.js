60bb8f415f5b9e0bc9f1b482f37b4dfac4c7fb4c
kalisowski <lisekgda3@gmail.com>
2022-12-23 16:44:50 +0100

    media width change

--- cocktail-catalog/tailwind.config.js
+++ cocktail-catalog/tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      sm: '576px',
+      md: '850px',
+      lg: '1250px',
+      xl: '1650px',
+      '2xl': '2050px',
+    },
     extend: {},
   },
   plugins: [require('daisyui')],
