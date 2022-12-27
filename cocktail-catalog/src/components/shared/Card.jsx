6c779ac5a7762a1aa46df05519301545ef65ff98
kalisowski <lisekgda3@gmail.com>
2022-12-27 20:36:01 +0100

    Card component for cocktail display

--- cocktail-catalog/src/components/shared/Card.jsx
+++ cocktail-catalog/src/components/shared/Card.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function Card() {
-  return <div>Card</div>;
+function Card({ id, name, image, category }) {
+  let navigate = useNavigate();
+  const handleClick = () => {
+    let path = `/cocktail/${id}`;
+    navigate(path);
+  };
+
+  return (
+    <div className="card-comact w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={image} alt="obrazek" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{category}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary" onClick={handleClick}>
+            Details
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
