ffa06d234e3030549e38934e05b4ce7b9d13a482
kalisowski <lisekgda3@gmail.com>
2022-12-27 20:34:59 +0100

    ui revamp

--- cocktail-catalog/tailwind.config.js
+++ cocktail-catalog/tailwind.config.js
@@ -12,4 +12,14 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: ['halloween', 'coffee', 'dark'],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dark',
+  },
 };
