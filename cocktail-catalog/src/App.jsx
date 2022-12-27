a96ffe5cbb3f856c8520406757a360dd4d7dc705
kalisowski <lisekgda3@gmail.com>
2022-12-27 18:33:17 +0100

    Basic Routing implemented

--- cocktail-catalog/src/App.jsx
+++ cocktail-catalog/src/App.jsx
@@ -1,9 +1,19 @@
-import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import CocktailInfo from './components/shared/CocktailInfo';
+import AdminPage from './pages/AdminPage';
+
 function App() {
   return (
-    <>
-      <NavBar />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route exact path="/cocktail/:id" element={<CocktailInfo />} />
+      </Routes>
+    </Router>
   );
 }
 
