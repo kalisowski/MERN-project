0e53b6e416413c5015dd19e77e356d0e3b677cd5
kalisowski <lisekgda3@gmail.com>
2022-12-27 18:30:08 +0100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/api/cocktails', require('./routes/cocktailsRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
