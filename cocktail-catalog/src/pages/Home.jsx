5a7491987f893b4ca6a18970f4fbfd3f915d9ae8
kalisowski <lisekgda3@gmail.com>
2022-12-29 23:23:46 +0100

    added abort controllers for cocktail fetching and props for Navbar

--- cocktail-catalog/src/pages/Home.jsx
+++ cocktail-catalog/src/pages/Home.jsx
@@ -1,24 +1,40 @@
 import React, { useState, useEffect } from 'react';
-// import Form from './components/Form';
 import NavBar from '../components/NavBar';
 import Card from '../components/shared/Card';
 
 function Home() {
+  const url = `${process.env.REACT_APP_API_COCKTAILS}`;
   const [cocktails, setCocktails] = useState([]);
 
-  const getCocktails = async () => {
-    const response = await fetch('http://localhost:5000/api/cocktails');
-    const data = await response.json();
-    setCocktails(data.cocktails);
+  const getCocktails = async (controller) => {
+    try {
+      const response = await fetch(url, {
+        signal: controller.signal,
+      });
+      const data = await response.json();
+      setCocktails(data.cocktails);
+    } catch (error) {
+      if (process.env.NODE_ENV === 'development') console.log(error);
+    }
   };
 
   useEffect(() => {
-    getCocktails();
+    const abortController = new AbortController();
+    getCocktails(abortController);
+    return () => {
+      abortController.abort();
+    };
+    // eslint disable next line is used to disable the warning that useEffect is missing a dependency
+    //eslint-disable-next-line
   }, []);
+
+  const updateCocktails = (newCocktails) => {
+    setCocktails(newCocktails);
+  };
 
   return (
     <div>
-      <NavBar />
+      <NavBar updateCocktails={updateCocktails} getCocktails={getCocktails} />
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10 m-10 justify-self-center justify-items-center">
         {cocktails.map((cocktail) => {
           return (
@@ -32,7 +48,6 @@ function Home() {
           );
         })}
       </div>
-      {/* <Form /> */}
     </div>
   );
 }
