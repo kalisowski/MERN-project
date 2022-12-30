2e9580424cd9376ad89c1f8002eba2f7c6e851a0
kalisowski <lisekgda3@gmail.com>
2022-12-30 04:50:28 +0100

    added path with images

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const cors = require('cors');
+const path = require('path');
 const port = process.env.PORT || 5000;
+
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 
@@ -13,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/cocktails', require('./routes/cocktailsRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
 app.use('/api/comments', require('./routes/commentsRoutes'));
