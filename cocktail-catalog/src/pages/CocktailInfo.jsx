16420d7b78e3549a59ac3e6957d0b379ca780322
kalisowski <lisekgda3@gmail.com>
2022-12-30 04:51:12 +0100

    moved CocktailInfo to pages since it is a page

--- cocktail-catalog/src/pages/CocktailInfo.jsx
+++ cocktail-catalog/src/pages/CocktailInfo.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 
 function CocktailInfo() {
   const [cocktail, setCocktail] = useState();
+  const [comments, setComments] = useState();
+
   const { id } = useParams();
 
   const getCocktail = () => {
-    const url = `${process.env.REACT_APP_API_COCKTAILS}/cocktail/${id}`;
+    const url = `${process.env.REACT_APP_API_COCKTAILS}cocktail/${id}`;
     axios.get(url).then((res) => {
       setCocktail(res.data);
     });
   };
 
+  const getComments = () => {
+    const url = `${process.env.REACT_APP_API_COMMENTS}/${id}`;
+    axios.get(url).then((res) => {
+      setComments(res.data);
+    });
+  };
+
   getCocktail();
+  getComments();
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -33,7 +43,7 @@ function CocktailInfo() {
           </div>
           <div className="mockup-window border bg-primary">
             <div className="flex flex-col justify-center px-4 py-16 bg-base-200">
-              <h1 className="text-4xl">Instructions:</h1>
+              <h1 className=" flex text-4xl">Instructions:</h1>
               <div>
                 <ul>
                   {cocktail.instructions.map((instruction, id) => {
@@ -61,15 +71,39 @@ function CocktailInfo() {
                   </div>
                 </div>
                 <div className="">
-                  <button className="btn bg-primary">
-                    <Link to="/">Take me back!</Link>
-                  </button>
+                  <Link to="/">
+                    <button className="btn bg-primary">Take me back!</button>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
       )}
+      <div>
+        {comments && (
+          <div className="flex flex-col h-0 mt-10">
+            <h1 className="text-4xl">Comments:</h1>
+            {comments.length === 0 ? (
+              <p>There are no comments yet!</p>
+            ) : (
+              comments.map((comment, id) => {
+                return (
+                  <div className="card w-100 bg-primary shadow-xl m-5" key={id}>
+                    <div className="card-body">
+                      <h1 className="card-title text-4xl">
+                        Comment: {comment.comment}
+                      </h1>
+                      <p className="text-2xl">Author: {comment.author}</p>
+                      <p className="text-2xl">Rating: {comment.rating}</p>
+                    </div>
+                  </div>
+                );
+              })
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
