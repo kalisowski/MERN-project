bdf576f850e7e2654b19962dd7090ad38e0abe9c
kalisowski <lisekgda3@gmail.com>
2023-01-05 01:31:08 +0100

    little update

--- cocktail-catalog/src/pages/AdminPage.jsx
+++ cocktail-catalog/src/pages/AdminPage.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AdminLogin from '../components/AdminLogin';
-import AddForm from '../components/AddForm';
+import { Link } from 'react-router-dom';
 
 function AdminPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <AddForm />
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <AdminLogin />
+        <Link to="/">
+          <div className="flex flex-start btn bg-warning m-10">
+            Take me back!
+          </div>
+        </Link>
+      </div>
+    </>
   );
-  //   return <AdminLogin />;
 }
 
 export default AdminPage;
