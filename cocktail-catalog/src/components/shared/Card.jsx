a80cc472ef3b84e1f65dc5f763810a98a5eb3882
kalisowski <lisekgda3@gmail.com>
2023-01-05 01:29:35 +0100

    WIP admin functionality - added buttons for admin

--- cocktail-catalog/src/components/shared/Card.jsx
+++ cocktail-catalog/src/components/shared/Card.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Card({ id, name, image, category }) {
+  const { user } = useSelector((state) => state.auth);
   let navigate = useNavigate();
   const handleClick = () => {
     let path = `/cocktail/${id}`;
     navigate(path);
+  };
+
+  const handleDelete = () => {
+    console.log('delete');
   };
 
   return (
@@ -17,6 +23,14 @@ function Card({ id, name, image, category }) {
         <h2 className="card-title">{name}</h2>
         <p>{category}</p>
         <div className="card-actions justify-end">
+          {user && (
+            <>
+              <button className="btn btn-error" onClick={handleDelete}>
+                Delete
+              </button>
+              <button className="btn btn-accent">Edit</button>
+            </>
+          )}
           <button className="btn btn-primary" onClick={handleClick}>
             Details
           </button>
