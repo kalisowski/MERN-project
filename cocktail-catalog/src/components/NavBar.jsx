effd733abb42c8823f9bbedec90e1330848eae28
kalisowski <lisekgda3@gmail.com>
2023-01-05 01:28:17 +0100

    patched for admin and swapped SVG for react-icons

--- cocktail-catalog/src/components/NavBar.jsx
+++ cocktail-catalog/src/components/NavBar.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import SearchSvg from './assets/Search.jsx';
 import _ from 'underscore';
+import { FiLogOut } from 'react-icons/fi';
+import { AiOutlineClear } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { logoutAdmin, reset } from '../reducers/authSlice';
+import { useDispatch } from 'react-redux';
 
 function NavBar(props) {
-  const { updateCocktails, updateLoadingState } = props;
+  const dispatch = useDispatch();
+  const { updateCocktails, updateLoadingState, main, admin } = props;
   const [search, setSearch] = useState('');
 
   const debouncedSearch = _.debounce(async (controller) => {
@@ -39,28 +44,53 @@ function NavBar(props) {
     setSearch('');
   };
 
+  const logOut = () => {
+    dispatch(logoutAdmin());
+    dispatch(reset());
+  };
+
   return (
     <div className="navbar bg-primary flex-none flex-wrap">
       <div className="flex-1">
         <a href="/" className="btn btn-ghost normal-case text-xl flex-initial ">
           Cocktails
         </a>
+        {admin && (
+          <>
+            <div className="flex-1">
+              <a
+                href="/add"
+                className="btn btn-ghost normal-case text-xl flex-initial "
+              >
+                Add new cocktail
+              </a>
+            </div>
+          </>
+        )}
       </div>
-      <div className="flex-none gap-2 flex max-w-full">
-        <input
-          type="text"
-          placeholder="Search"
-          className="form-control flex-auto input input-bordered min-w-0"
-          onChange={handleSearchChange}
-          value={search}
-        />
-        <button
-          className="btn-ghost btn-circle btn flex-initial shrink-0 mr-5 mx-3"
-          onClick={handleSearch}
-        >
-          <SearchSvg />
-        </button>
-      </div>
+      {main && (
+        <div className="flex-none flex max-w-full">
+          <input
+            type="text"
+            placeholder="Search"
+            className="form-control flex-auto input input-bordered min-w-0"
+            onChange={handleSearchChange}
+            value={search}
+          />
+          <button
+            className="btn-ghost btn-circle btn flex-initial shrink-0 mr-3 mx-3"
+            onClick={handleSearch}
+          >
+            <AiOutlineClear size="2.5em" />
+          </button>
+          {admin && (
+            <div className="btn" onClick={logOut}>
+              <p className="mr-2">Log Out</p>
+              <FiLogOut />
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
