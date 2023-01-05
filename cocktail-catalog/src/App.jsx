f538b32206cbeb146e2b3dea02bc97a7efccbefd
kalisowski <lisekgda3@gmail.com>
2023-01-05 01:25:42 +0100

    added toastify for displaying login errors

--- cocktail-catalog/src/App.jsx
+++ cocktail-catalog/src/App.jsx
@@ -1,19 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import CocktailInfo from './pages/CocktailInfo';
 import AdminPage from './pages/AdminPage';
+import AddPage from './pages/AddPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route exact path="/cocktail/:id" element={<CocktailInfo />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route exact path="/cocktail/:id" element={<CocktailInfo />} />
+          <Route path="/add" element={<AddPage />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
