6d14e8f73e0113a48680a8ab3ced9c87f3124d94
kalisowski <lisekgda3@gmail.com>
2023-01-12 01:02:33 +0100

    allowing cookies to be saved and using cookieparser

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const colors = require('colors');
 const cors = require('cors');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -14,7 +15,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/cocktails', require('./routes/cocktailsRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
