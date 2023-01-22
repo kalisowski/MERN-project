f5d3a2dde0af372a0fe4e6597c996e5ee248551c
kalisowski <lisekgda3@gmail.com>
2023-01-22 15:55:46 +0100

    new routes for import/export

--- backend/routes/cocktailsRoutes.js
+++ backend/routes/cocktailsRoutes.js
@@ -7,7 +7,9 @@ const {
   deleteCocktail,
   findCocktail,
   findCocktailByName,
+  newCocktailsFromJson,
   upload,
+  clearCocktails,
 } = require('../controllers/cocktailsController');
 
 router.route('/').get(getCocktails).post(upload.single('image'), setCocktail);
@@ -17,4 +19,6 @@ router
   .put(upload.single('image'), updateCocktail)
   .delete(deleteCocktail);
 router.route('/search').get(findCocktailByName);
+router.route('/import').post(newCocktailsFromJson).delete(clearCocktails);
+router.route('/import-single').post(newCocktailsFromJson);
 module.exports = router;
